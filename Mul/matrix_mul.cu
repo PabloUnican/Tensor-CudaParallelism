f9e17cde7e513305e801c42afbf1b6c04768bbc0
PabloUnican <pss542@alumnos.unican.es>
2025-03-16 19:42:07 +0100

    [FEATURE] Support larger matrices and optimize performance using Tensor Cores

--- Mul/matrix_mul.cu
+++ Mul/matrix_mul.cu
@@ -4,18 +4,59 @@
 #include <cuda_fp16.h>
 #include <time.h>
 
-#define N 8192 // Size of the matrices
+#define N 16384 // Size of the matrices
 
-__global__ void matrixMulKernel(half* d_A, half* d_B, float* d_C, int width) {
+__global__ void matrixMulKernel(half* d_A, half* d_B, float* d_C, int width, float coeficient) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     half sum = 0;
 
-    if (row < width && col < width) {
-        for (int k = 0; k < width; k++) {
-            sum += d_A[row * width + k] * d_B[k * width + col];
+    int numTensor = coeficient * width;
+    if (row >= numTensor) {
+        if (row < width && col < width) {
+            for (int k = 0; k < width; k++) {
+                sum += d_A[row * width + k] * d_B[k * width + col];
+            }
+            d_C[row * width + col] = __half2float(sum);
         }
-        d_C[row * width + col] = __half2float(sum);
+    } else {
+        int lda = width;
+        int ldb = width;
+        int ldc = width;
+
+        // Tile using a 2D grid
+        int warpM = (blockIdx.x * blockDim.x + threadIdx.x) / 32;
+        int warpN = (blockIdx.y * blockDim.y + threadIdx.y) / 8;
+
+        // Declare the fragments
+        nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 16, 16, 16, half, nvcuda::wmma::row_major> a_frag;
+        nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 16, 16, 16, half, nvcuda::wmma::row_major> b_frag;
+        nvcuda::wmma::fragment<nvcuda::wmma::accumulator, 16, 16, 16, float> c_frag;
+
+        nvcuda::wmma::fill_fragment(c_frag, 0.0f);
+
+        // Loop over k
+        for (int i = 0; i < numTensor; i += 16) {
+            int aRow = warpM * 16;
+            int aCol = i;
+            int bRow = i;
+            int bCol = warpN * 16;
+
+            if (aRow < width && aCol < width && bRow < width && bCol < width) {
+                // Load the inputs
+                nvcuda::wmma::load_matrix_sync(a_frag, d_A + aRow * lda + aCol, lda);
+                nvcuda::wmma::load_matrix_sync(b_frag, d_B + bRow * ldb + bCol, ldb);
+
+                // Perform the matrix multiplication
+                nvcuda::wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+            }
+        }
+
+        // Load in the current value of d_C, scale it by beta, and add this our result scaled by alpha
+        int cRow = warpM * 16;
+        int cCol = warpN * 16;
+        // Store the output
+        nvcuda::wmma::store_matrix_sync(d_C + cRow * ldc + cCol, c_frag, ldc, nvcuda::wmma::mem_row_major);
     }
 }
 
@@ -77,7 +118,7 @@ void matrixMul(half* h_A, half* h_B, float* h_C, int width) {
     dim3 blocksPerGrid((width + threadsPerBlock.x - 1) / threadsPerBlock.x, 
                        (width + threadsPerBlock.y - 1) / threadsPerBlock.y);
 
-    matrixMulTensorCore<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, width);
+    matrixMulKernel<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, width, 1.0);
     cudaDeviceSynchronize();
 
     cudaMemcpy(h_C, d_C, sizeOut, cudaMemcpyDeviceToHost);
